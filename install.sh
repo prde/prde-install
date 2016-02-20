c36f90c599085c23350a528578facc38ee5951fe
Sergey Sobko <S.Sobko@profitware.ru>
2016-02-20 04:58:39 -0800

    installation script - fixes

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-sudo dnf install -y ansible
-ansible-pull --url=https://github.com/prde/prde-install-fedora.git -K -i <(echo localhost ansible_connection=local)
+ANSIBLE_PULL=/usr/bin/ansible-pull
+
+[ -x $ANSIBLE_PULL ] || sudo dnf install -y ansible
+
+if [ -x $ANSIBLE_PULL ]
+then
+    $ANSIBLE_PULL --url=https://github.com/prde/prde-install-fedora.git \
+                  -K -i <(echo localhost ansible_connection=local)
+else
+    echo "Cannot find or install Ansible. Exiting."
+fi
