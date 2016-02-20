4b0ce03aaa3c9ad4a75c6b95dd8fec3659bb19e7
bug2bug <S.Sobko@profitware.ru>
2016-02-21 01:34:15 +0300

    local node inventory file

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ ANSIBLE_PULL=/usr/bin/ansible-pull
 
 if [ -x $ANSIBLE_PULL ]
 then
-    $ANSIBLE_PULL --url=https://github.com/prde/prde-install-fedora.git \
-                  -K -i <(echo localhost ansible_connection=local)
+    echo "localhost ansible_connection=local" > /tmp/local_node_inventory
+    $ANSIBLE_PULL --url=https://github.com/prde/prde-install-fedora.git -K -i /tmp/local_node_inventory
 else
     echo "Cannot find or install Ansible. Exiting."
 fi
